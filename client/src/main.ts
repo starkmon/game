ff9d77fc6432f98ba490722af72471057bd8e050
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-08-31 08:06:35 +0800

    Minor updates

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import logo from '/logo.svg';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="https://dojoengine.org" target="_blank">
       <img src="${logo}" class="logo" alt="Vite logo" />
     </a>
     <h1>Vite + TypeScript</h1>
