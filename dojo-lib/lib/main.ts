cc5764b4e52e15d3b2578b5c3a1336090c6c64e1
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-08-31 08:35:44 +0800

    feat: fetch entity

--- dojo-lib/lib/main.ts
+++ dojo-lib/lib/main.ts
@@ -1,5 +1,5 @@
 import { Account, BigNumberish, RpcProvider } from "starknet";
-import { RPCProvider, Query } from "@dojoengine/core";
+import { RPCProvider, Query, strTofelt252Felt } from "@dojoengine/core";
 
 type DojoConstructorArgs = {
 	account_address: string,
@@ -9,17 +9,39 @@ type DojoConstructorArgs = {
 };
 
 export default class Dojo {
-	provider: RpcProvider;
+	sn_provider: RpcProvider;
 	account: Account;
-	dojo_provider: RPCProvider;
+	provider: RPCProvider;
+	world: string;
 
 	constructor(args: DojoConstructorArgs) {
-		this.provider = new RpcProvider({ nodeUrl: args.nodeUrl || 'http://localhost:5050' });
-		this.account = new Account(this.provider, args.account_address, args.account_private_key);
-		this.dojo_provider = new RPCProvider(args.world_address, args.nodeUrl);
+		this.world = args.world_address;
+		this.sn_provider = new RpcProvider({ nodeUrl: args.nodeUrl || 'http://localhost:5050' });
+		this.account = new Account(this.sn_provider, args.account_address, args.account_private_key);
+		this.provider = new RPCProvider(args.world_address, args.nodeUrl);
 	}
 
-	execute(system: string, calldata: BigNumberish[]) {
-		this.dojo_provider.execute(this.account, system, calldata);
+	execute(system: string, calldata?: BigNumberish[]) {
+		return this.provider.execute(this.account, system, calldata || []);
+	}
+
+	call(system: string, calldata: BigNumberish[] = []) {
+		calldata = [strTofelt252Felt(system), calldata.length, ...calldata]
+		return this.sn_provider.callContract({
+			contractAddress: this.world,
+			calldata: calldata,
+			entrypoint: 'execute',
+		});
+	}
+
+	entity(component: string, keys: BigNumberish[], offset: number = 0, length: number = 0) {
+
+		const keys_: BigNumberish[] = typeof keys === 'string' ? [keys] : keys;
+		const calldata = [strTofelt252Felt(component), keys_.length, ...keys_, offset, length];
+		return this.sn_provider.callContract({
+			contractAddress: this.world,
+			calldata: calldata,
+			entrypoint: 'entity',
+		});
 	}
 }
--- dojo-lib/src/main.ts
+++ dojo-lib/src/main.ts
@@ -6,13 +6,18 @@ declare global { var dojo: Dojo; }
 const $f = document.querySelector('form#app');
 
 if ($f) {
-	$f.addEventListener('submit', e => {
-		e.preventDefault();
+	function setup($f: Element) {
 		let d = new FormData($f);
 		window.dojo = new Dojo({
 			account_address: d.get('account')?.toString() || '',
 			account_private_key: d.get('skey')?.toString() || '',
 			world_address: d.get('world')?.toString() || '',
 		});
+
+	}
+	$f.addEventListener('submit', e => {
+		e.preventDefault();
+		setup($f);
 	});
+	setTimeout(() => setup($f), 250);
 }
