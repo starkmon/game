b93785d7a173f758ae19086f18256f784a3d1f2c
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-10-21 20:45:43 +0700

    map: render

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -2,20 +2,70 @@
 
 import * as Phaser from 'phaser';
 
-export default class GameScene extends Phaser.Scene {
-	constructor() {
-		super('GameScene');
-	}
+export default class Example extends Phaser.Scene {
+	tiles = [7, 7, 7, 6, 6, 6, 0, 0, 0, 1, 1, 2, 3, 4, 5];
+	distance = 0;
+	mapHeight = 38;
+	mapWidth = 52;
+	sx = 0;
+	text = "";
+	map = [];
 
 	preload() {
-		// Add any assets you want to preload here
+		this.load.image('tiles', 'assets/ground.png');
 	}
 
 	create() {
-		// Add your game logic here
+		const mapData = [];
+
+		for (let y = 0; y < this.mapHeight; y++) {
+			const row = [];
+
+			for (let x = 0; x < this.mapWidth; x++) {
+				//  Scatter the tiles so we get more mud and less stones
+				const tileIndex = Phaser.Math.RND.weightedPick(this.tiles);
+
+				row.push(tileIndex);
+			}
+
+			mapData.push(row);
+		}
+
+		this.map = this.make.tilemap({ data: mapData, tileWidth: 16, tileHeight: 16 });
+
+		const tileset = this.map.addTilesetImage('tiles');
+		const layer = this.map.createLayer(0, tileset, 0, 0);
+
 	}
 
-	update() {
-		// Add your game update logic here
+	update(time, delta) {
+		//  Any speed as long as 16 evenly divides by it
+		this.sx += 4;
+
+		this.distance += this.sx;
+
+		if (this.sx === 16) {
+			//  Reset and create new strip
+
+			let tile;
+			let prev;
+
+			for (let y = 0; y < this.mapHeight; y++) {
+				for (let x = 1; x < this.mapWidth; x++) {
+					tile = this.map.getTileAt(x, y);
+					prev = this.map.getTileAt(x - 1, y);
+
+					prev.index = tile.index;
+
+					if (x === this.mapWidth - 1) {
+						tile.index = Phaser.Math.RND.weightedPick(this.tiles);
+					}
+				}
+			}
+
+			this.sx = 0;
+		}
+
+		this.cameras.main.scrollX = this.sx;
 	}
 }
