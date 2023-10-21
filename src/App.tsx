2ae7f5db0016eb292fa71ddfd839ef165ca518b8
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-10-21 18:41:48 +0700

    init: phaser

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useRef } from 'react';
+import * as Phaser from 'phaser';
+import GameScene from './scenes/GameScene';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (myRef.current) {
+      const gameConfig: Phaser.Types.Core.GameConfig = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scene: [GameScene], // Add your game scenes here
+        parent: myRef.current,
+      };
+
+      const game = new Phaser.Game(gameConfig);
+
+      return () => {
+        game.destroy(true);
+      };
+    }
+  }, [myRef]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App" ref={myRef}>
+      {/* Your React components */}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/scenes/GameScene.ts
@@ -0,0 +1,21 @@
+// src/scenes/GameScene.ts
+
+import * as Phaser from 'phaser';
+
+export default class GameScene extends Phaser.Scene {
+	constructor() {
+		super('GameScene');
+	}
+
+	preload() {
+		// Add any assets you want to preload here
+	}
+
+	create() {
+		// Add your game logic here
+	}
+
+	update() {
+		// Add your game update logic here
+	}
+}
