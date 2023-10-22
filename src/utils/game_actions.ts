e17ce65695684b1bde36fb9078b008bac26d62e3
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-10-22 07:15:51 +0700

    fix: game crash

--- src/utils/game_actions.ts
+++ src/utils/game_actions.ts
@@ -1,12 +1,7 @@
 import { creatureOnCoordinates } from "./reveal_creatures";
-import { BigNumberish } from "starknet";
 
 export function lookup_creature_on_coordinates(x: number, y: number) {
-    const CREATURE_SEED: BigNumberish = '0xB1B89B84BB6F354B8568285C6ECAFED311AFF90B0A83462164ED0A7A06A6F';
-    const PROBABILITY: BigNumberish = '0x100000'; // Out of 1
-    const xToBigInt = Math.round(x) as BigNumberish;
-    const yToBigInt = Math.round(y) as BigNumberish;
-    return creatureOnCoordinates(CREATURE_SEED, PROBABILITY, xToBigInt , yToBigInt);
+    return creatureOnCoordinates(Math.round(x), Math.round(y));
 }
 
 export function claim_creature_on_coordinates(x: number, y: number) {
